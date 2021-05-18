74f774f94b0379780a0f17979a21f278754a2c63
Colin McFadden <cmcfadden@gmail.com>
2021-05-18 16:47:06 -0500

    make emplid fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
      protected $fillable = [
-        'name', 'email', 'umndid'
+        'name', 'email', 'umndid', 'emplid'
     ];
 
     /**
